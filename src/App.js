9f92fee9aaf511126d1b92adabba8245d42fa4c7
Lem <lemuel.dulfo@gmail.com>
2020-09-22 17:26:39 -0600

    Adding todo using redux

--- src/App.js
+++ src/App.js
@@ -3,8 +3,91 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import { MyCounter } from './features/counter/MyCounter';
+import {createStore, combineReducers} from 'redux';
 
 function App() {
+
+  const todo = (state, action) => {
+    switch (action.type) {
+      case 'ADD_TODO':
+        return {
+          id: action.id,
+          text: action.text,
+          completed: false
+        };
+      case 'TOGGLE_TODO':
+        if (state.id !== action.id) {
+          return state;
+        }
+
+        return {
+          ...state,
+          completed: !state.completed
+        }
+      default:
+        return state;
+    }
+  }
+
+  const todos = (state = [], action) => {
+    switch (action.type) {
+      case 'ADD_TODO': 
+        return [
+          ...state,
+          todo(undefined, action)
+        ];
+      case 'TOGGLE_TODO': {
+        return state.map(t => todo(t, action));
+      }
+      default:
+        return state;
+    }
+  }
+
+  const visibilityFilter = (state = 'SHOW_ALL', action) => {
+    switch (action.type) {
+      case 'SET_VISIBILITY_FILTER':
+        return action.filter;
+      default:
+        return state;
+    }
+  }
+
+  // Reducer composition - manual way
+  // const todoApp = (state = {}, action) => {
+  //   return ({
+  //     todos: todos(state.todos, action),
+  //     visibilityFilter: visibilityFilter(
+  //       state.visibilityFilter,
+  //       action
+      
+  //     )
+  //   }
+  //   )
+  // }
+
+  // Reducer composition using combineReducers
+  // keys here are the fields of the state object (e.g. state.todos becomes todos:)
+  const todoApp = combineReducers({
+    todos: todos,
+    visibilityFilter: visibilityFilter
+    // in shorthand notation it just becomes
+    // todos,
+    // visibilityFilter
+  });
+
+  
+
+
+  // state = todos(state, {type: 'ADD_TODO', id: 0, text: 'Learn_Redux'});
+  // state = todos(state, {type: 'TOGGLE_TODO', id: 0,});
+  const store = createStore(todoApp);
+  store.dispatch({type: 'ADD_TODO', id: 0, text: 'Learn_Redux'});
+  store.dispatch({type: 'TOGGLE_TODO', id: 0})
+  store.dispatch({type: 'SET_VISIBILITY_FILTER', filter: 'SHOW_COMPLETED'});
+
+  console.log(JSON.stringify(store.getState()));
+
   return (
     <div className="App">
       <header className="App-header">
--- src/features/counter/MyCounter.js
+++ src/features/counter/MyCounter.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import {useSelector} from 'react-redux';
-import {selectCount} from './counterSlice';
+import {useSelector, useDispatch} from 'react-redux';
+import {selectCount, increment} from './counterSlice';
 
 export function MyCounter() {
     const count = useSelector(selectCount);
+    const dispatch = useDispatch();
     return (
         <div>
             MyCounter does work {count}
+            <button onClick={() => dispatch(increment())}>increment</button>
         </div>
     )
 }
