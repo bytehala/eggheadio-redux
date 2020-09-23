f82d97419a658e62c83b3fb3b605c47ce03c0e21
Lem <lemuel.dulfo@gmail.com>
2020-09-23 00:38:19 -0600

    Visibility filter start

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { combineReducers, createStore } from "redux";
 
-import {combineReducers, createStore} from 'redux';
-
-const todo = (state, action) => {
+const todo = (state = {}, action) => {
   switch (action.type) {
     case "ADD_TODO":
       return {
         id: action.id,
         text: action.text,
-        completed: false,
+        completed: action.completed,
       };
     case "TOGGLE_TODO":
       if (state.id !== action.id) {
@@ -32,15 +29,15 @@ const todos = (state = [], action) => {
   switch (action.type) {
     case "ADD_TODO":
       return [...state, todo(undefined, action)];
-    case "TOGGLE_TODO": {
-      return state.map((t) => todo(t, action));
-    }
+    case "TOGGLE_TODO":
+      return state.map((td) => todo(td, action));
     default:
       return state;
   }
 };
 
 const visibilityFilter = (state = "SHOW_ALL", action) => {
+  console.log(action.filter);
   switch (action.type) {
     case "SET_VISIBILITY_FILTER":
       return action.filter;
@@ -49,56 +46,85 @@ const visibilityFilter = (state = "SHOW_ALL", action) => {
   }
 };
 
-
-const todoApp = combineReducers({todos, visibilityFilter});
+const todoApp = combineReducers({ todos, visibilityFilter });
 const store = createStore(todoApp);
 
-let nextTodoId = 0;
-class TodoApp extends Component {
-  render() {
+const FilterLink = ({filter, children}) => {
   return (
-    
-      <div>
-      <input ref={node => {
-        this.input = node;
-      }} />
-      <button
-        onClick={() => {
-          store.dispatch({
-            type: "ADD_TODO",
-            text: this.input.value,
-            id: nextTodoId++,
-          });
-          this.input.value = ''
-        }}
-      >
-        Add Todo
-      </button>
-      <ul>
-          {this.props.todos.map(
-              todo =>
-              <li key={todo.id}>{todo.text}</li>
-          )}
-      </ul>
-    </div>
-  
+    <a href='#'
+     onClick={e => {
+       e.preventDefault();
+       store.dispatch({
+         type: 'SET_VISIBILITY_FILTER',
+         filter
+       })
+     }}>{children}</a>
   )
-          }
 }
 
-const render = () => {
+let todoIndex = 0;
+class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        <input
+          ref={(node) => {
+            this.input = node;
+          }}
+        />
+        <button
+          onClick={() => {
+            store.dispatch({
+              type: "ADD_TODO",
+              id: todoIndex++,
+              text: this.input.value,
+              completed: false,
+            });
+            this.input.value = '';
+          }}
+        >
+          Add Todo
+        </button>
+        
+        <ul>
+          {this.props.todos.map((td) => (
+            <li
+              key={td.id}
+              onClick={() => {
+                store.dispatch({ type: "TOGGLE_TODO", id: td.id });
+              }}
+              style={{
+                textDecoration: td.completed? 'line-through' : 'none'
+              }}
+            >
+              {td.text}
+            </li>
+          ))}
+        </ul>
+
+        <p>
+          Show: {' '}
+          <FilterLink filter='SHOW_ALL'>All</FilterLink>
+          {' '}
+          <FilterLink filter='SHOW_COMPLETED'>Completed</FilterLink>
+          {' '}
+          <FilterLink filter='SHOW_ACTIVE'>Active</FilterLink>
+          {' '}
+        </p>
+      </div>
+    );
+  }
+}
+
+function render() {
+
   ReactDOM.render(
-    <React.StrictMode>
-        <TodoApp todos={store.getState().todos}/>
-    </React.StrictMode>,
-    document.getElementById('root')
+    <div>
+      <TodoApp todos={store.getState().todos} />
+    </div>,
+    document.getElementById("root")
   );
 }
 
 store.subscribe(render);
 render();
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
